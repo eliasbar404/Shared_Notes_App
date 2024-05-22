3ff6f965a152e8ab7e52b92fc9142341096ebb8a
ilyas <ibaraaouche@gmail.com>
2024-05-22 11:36:47 +0100

    connection with database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,22 @@
 import express from "express"
+import dotenv from "dotenv"
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+
+dotenv.config();
+
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
+
+
+
+// app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  connectToMongoDB()
+  console.log(`Example app listening on port ${PORT}`)
 })
